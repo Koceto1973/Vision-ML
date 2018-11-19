896e7db4f5daf9cda9d6d1f0f3f79a56cd99a2ab
Koceto1973 <koceto1973@yahoo.ca>
2018-11-19 14:52:41 -0800

    initial commit

---
+++ Vision-ML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vision-ML
+//
+//  Created by K.K. on 19.11.18.
+//  Copyright © 2018 K.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
