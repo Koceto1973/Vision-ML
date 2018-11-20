930ce616c95d7926efe6ae391a7be1464e57dcfa
Koceto1973 <koceto1973@yahoo.ca>
2018-11-19 17:48:59 -0800

    squeeze net in

--- Vision-ML/Controller/CameraVC.swift
+++ Vision-ML/Controller/CameraVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
 
 class CameraVC: UIViewController {
     
@@ -95,6 +97,28 @@ class CameraVC: UIViewController {
         
         cameraOutput.capturePhoto(with: settings, delegate: self)
     }
+    
+    func resultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        
+        for classification in results {
+            if classification.confidence < 0.5 {
+                let unknownObjectMessage = "I'm not sure what this is. Please try again."
+                self.identificationLbl.text = unknownObjectMessage
+                synthesizeSpeech(fromString: unknownObjectMessage)
+                self.confidenceLbl.text = ""
+                break
+            } else {
+                let identification = classification.identifier
+                let confidence = Int(classification.confidence * 100)
+                self.identificationLbl.text = identification
+                self.confidenceLbl.text = "CONFIDENCE: \(confidence)%"
+                let completeSentence = "This looks like a \(identification) and I'm \(confidence) percent sure."
+                synthesizeSpeech(fromString: completeSentence)
+                break
+            }
+        }
+    }
 }
 
 extension CameraVC: AVCapturePhotoCaptureDelegate {
@@ -104,14 +128,15 @@ extension CameraVC: AVCapturePhotoCaptureDelegate {
         } else {
             photoData = photo.fileDataRepresentation()
             
-//            do {
-//                let model = try VNCoreMLModel(for: SqueezeNet().model)
-//                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
-//                let handler = VNImageRequestHandler(data: photoData!)
-//                try handler.perform([request])
-//            } catch {
-//                debugPrint(error)
-//            }
+            // photo data goes as input to SqeezeNet
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                try handler.perform([request])
+            } catch {
+                debugPrint(error)
+            }
             
             let image = UIImage(data: photoData!)
             self.captureImageView.image = image
